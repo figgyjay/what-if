0a828ab54649c06e7a4849f30a110d6eda56a53a
figgyjay <79807654+figgyjay@users.noreply.github.com>
2022-06-06 00:36:23 -0500

    Add files via upload

---
+++ group-sizes.js
@@ -0,0 +1,10 @@
+const prompt = require('prompt-sync')({sigint: true})
+
+let num1 = Number(prompt (" What is the size of your class? "))
+
+if (num1 /= 3){
+    console.log(" Number of Groups")
+}
+else if(num1 /= 2){
+    console.log(" Number of Groups")
+}
