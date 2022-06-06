58be84662cc30eaa3b31f1c7ff081a6c61f8db63
figgyjay <79807654+figgyjay@users.noreply.github.com>
2022-06-06 01:09:55 -0500

    Add files via upload

--- weekday.js
+++ weekday.js
@@ -2,26 +2,25 @@ const prompt = require('prompt-sync')({sigint: true})
 
 let num1 = Number(prompt(" Enter a number from 1 - 7 to find out what's your favorite day of the week :"))
 
-if (num1 = 1){
+if (num1 ===1){
     console.log("Monday")
 }
-if (num1 = 2){
+if (num1 === 2){
     console.log("Tuesday")
 }
-if (num1 = 3){
+if (num1 === 3){
     console.log("Wedensday")
 }
-
-if(num1 = 4){
+if(num1 === 4){
     console.log("Thursday")
 }
-if (num1 = 5){
+if (num1 === 5){
     console.log("Friday")
 }
-if (num1 = 6){
+if (num1 === 6){
     console.log("Saturday")
 }
-if(num1 = 7){
+if(num1 === 7){
     console.log("Sunday")
 }
 else {
