37afc06e729a11d305ffe94d46e216fed3f051b2
figgyjay <79807654+figgyjay@users.noreply.github.com>
2022-06-06 00:52:57 -0500

    Add files via upload

--- what-can-you-do.js
+++ what-can-you-do.js
@@ -1,19 +1,23 @@
 const prompt = require('prompt-sync')({sigint: true})
 
 
-let num1 = Number(prompt(" What can you do? Enter your age to find out: "))
+let age = Number(prompt(" What can you do? Enter your age to find out: "))
 
-if (num1 < 16){
+if (age < 16){
     console.log(" You can not drive. Life sucks for you. ")
 }
 
-else if (num1 > 16){
+else if (age === 16){
     console.log(" You can drive but not vote. ")
 }
-else if(num1 >= 18){
-    console.log(" You can vote but not rent a car.")
+else if(age === 17){
+    console.log(" You can drive but not vote. ")
 }
-else if (num1 >= 25){
+else if (age <= 24){
+    console.log(" You can vote but not rent a car. ")
+
+}
+else {
     console.log(" You can pretty much do anything. ")
 }
 
